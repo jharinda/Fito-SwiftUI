170f6c8ae92d7e2162dc95ae0332f41f5cc58ff7
Janith <jharinda@gmail.com>
2023-08-11 02:00:45 +0800

    Initial Commit

---
+++ Fito-SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Fito-SwiftUI
+//
+//  Created by user234937 on 8/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fito-SwiftUI/Fito_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fito_SwiftUIApp.swift
+//  Fito-SwiftUI
+//
+//  Created by user234937 on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Fito_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
