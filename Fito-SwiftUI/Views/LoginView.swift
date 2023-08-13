b9d05af18785703ac0f741b958da8d0bbdeb2818
Janith Harinda <jharinda@gmail.com>
2023-08-14 04:58:54 +0800

    add async calls to register and login

--- Fito-SwiftUI/Views/LoginView.swift
+++ Fito-SwiftUI/Views/LoginView.swift
@@ -9,19 +9,24 @@ import SwiftUI
 
 struct LoginView: View {
     var actionPerformed:((ProjectAction) -> Void)?
+    @AppStorage("currentUserEmail") var currentUserEmail = ""
     @State var email = "";
     @State var password = "";
+    @State var errorMessage = ""
+    
+    @State var isLoggedIn = false
     var body: some View {
         ZStack{
             Color.yellow.ignoresSafeArea()
             VStack{
                 Text("Login").font(.custom("Poppins-BoldItalic", size: 30)).offset(y:-40)
-                    
-                    TextField("Email",text:$email).font(.custom("Poppins-SemiBold", size: 15)).textFieldStyle(OvalTextFieldStyle())
+                
+                TextField("Email",text:$email).font(.custom("Poppins-SemiBold", size: 15)).textFieldStyle(OvalTextFieldStyle())
+                    .keyboardType(.emailAddress)
                     .padding(.vertical,0)
                     .padding(.horizontal,40)
                 
-                TextField("Password",text:$password).font(.custom("Poppins-SemiBold", size: 15)).textFieldStyle(OvalTextFieldStyle())
+                SecureField("Password",text:$password).font(.custom("Poppins-SemiBold", size: 15)).textFieldStyle(OvalTextFieldStyle())
                     .padding(.vertical,0)
                     .padding(.horizontal,40)
                 
@@ -38,9 +43,9 @@ struct LoginView: View {
                 }.padding(.vertical,20)
                 Button(action: {
                     print("Login clicked")
-                   login()
+                    login()
                 }) {
-                   
+                    
                     Text("Login").font(.custom("Poppins-SemiBold", size: 15)).padding(.horizontal,70)
                 }
                 .padding()
@@ -49,15 +54,80 @@ struct LoginView: View {
                 .cornerRadius(57)
                 .offset(y:5)
                 
+                NavigationLink(destination: ProfileView().navigationBarBackButtonHidden(true), isActive: $isLoggedIn) { EmptyView() }
+                
+                if(errorMessage != ""){
+                    Text(errorMessage).padding(10).foregroundColor(.red).padding(.horizontal,30)
+                        .background(.white).fontWeight(.bold).cornerRadius(60).offset(y:20)
+                }
+                
             }
         }
     }
     
     func login(){
-        var user = User(id: 0, name: "", password: password, email: email, age: 0, heightInCm: 0)
+        errorMessage = ""
+        
+        let res = loginAsync(email: email.lowercased(), password: password)
         
     }
     
+    func loginAsync(email:String, password:String) -> Bool{
+        @AppStorage("currentUserId") var currentUserId = 0
+        currentUserId = 0;
+        var tempUser = User(id: 0, name: "", password:password, email: email, age: 0, heightInCm: 0, gender: "")
+        var status = ""
+        guard let url = URL(string: USER_API_URL + "/validate") else {
+            status = "Invalid URL"
+            return false
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        do {
+            let jsonData = try JSONEncoder().encode(tempUser)
+            request.httpBody = jsonData
+        } catch {
+            status = "Encoding error: \(error)"
+            return false
+        }
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                status = "Network error: \(error)"
+                return
+            }
+            
+            if let data = data {
+                do {
+                    let user = try JSONDecoder().decode(User.self, from: data)
+                    
+                    currentUserId = user.id
+                    currentUserEmail = user.email
+                    isLoggedIn = true
+                    
+                    print(user)
+                    print("user found")
+                } catch {
+                    status = "Decoding error: \(error)"
+                    errorMessage = "Invalid Email or Password"
+                    return ;
+                }
+            }else{
+                errorMessage = "Invalid Email or Password"
+            }
+        }.resume()
+        print("currentUserId " + String(currentUserId))
+        if(currentUserId != 0){
+            print("User was logged in")
+            return true
+        }
+        
+        print("User wasn't logged in")
+        return false
+    }
 }
 
 struct OvalTextFieldStyle: TextFieldStyle {
@@ -68,6 +138,6 @@ struct OvalTextFieldStyle: TextFieldStyle {
             .background()
             .cornerRadius(15)
             .shadow(color: Color.black.opacity(0.1),radius: 10,x: 4,y:5)
-            
+        
     }
 }
